24212e0314ede529941b45e1d423c23c71ac38a0
GombarZolta <gombarzolta@gmail.com>
2025-04-22 08:25:36 +0200

    Fixed Lombok

--- src/main/java/com/esen/bookstore/model/Book.java
+++ src/main/java/com/esen/bookstore/model/Book.java
@@ -8,24 +8,17 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Data
+
 @Entity
+@Data
 public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Getter
-    @Setter
     private String title;
-    @Getter
-    @Setter
     private String author;
-    @Getter
-    @Setter
     private String publisher;
-    @Getter
-    @Setter
     private Double price;
 
     public Book() {
--- src/main/java/com/esen/bookstore/model/BookStore.java
+++ src/main/java/com/esen/bookstore/model/BookStore.java
@@ -1,34 +1,29 @@
 package com.esen.bookstore.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 //import lombok.Data;
 import java.util.Map;
 
-//@Data
+
 @Entity
+@Data
 public class BookStore {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column(unique = true)
     private String location;
     private Double priceModifyer;
     private Double moneyInCashRegister;
-
-
     @ElementCollection(fetch = FetchType.EAGER)
+    @JsonIgnore
     private Map<Book, Integer> inventory;
-
-    public BookStore(long id, String location, Double priceModifier, Double moneyInCashRegister, Map<Book, Integer> inventory) {
-        this.id = id;
-        this.location = location;
-        this.priceModifyer = priceModifier;
-        this.moneyInCashRegister = moneyInCashRegister;
-        this.inventory = inventory;
-    }
 
     public BookStore() {
     }
@@ -41,36 +36,12 @@ public class BookStore {
         this.id = id;
     }
 
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
     public Double getPriceModifier() {
         return priceModifyer;
     }
 
     public void setPriceModifier(Double priceModifier) {
         this.priceModifyer = priceModifier;
-    }
-
-    public Double getMoneyInCashRegister() {
-        return moneyInCashRegister;
-    }
-
-    public void setMoneyInCashRegister(Double moneyInCashRegister) {
-        this.moneyInCashRegister = moneyInCashRegister;
-    }
-
-    public Map<Book, Integer> getInventory() {
-        return inventory;
-    }
-
-    public void setInventory(Map<Book, Integer> inventory) {
-        this.inventory = inventory;
     }
 
 }
